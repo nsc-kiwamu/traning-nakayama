f273319b877cb3202a5ce1b80da9746a66b652bc
中山裕人 <@DESKTOP-T0HONAA>
2025-12-19 17:49:34 +0900

    問題1　再提出

--- first_traning/src/change/Problem01.java
+++ first_traning/src/change/Problem01.java
@@ -32,45 +32,10 @@ public class Problem01 {
 
         /* -- ここから問題 -- */
         // 3の倍数の一覧を出力
-        
-        /**
-         * 引数で受け取った数値から3の倍数のみを出力する。
-         * @param dataList 振り分け対象データ
-         */
-        public static void outputAnyNumber(List<Integer> dataList) {
-
-            System.out.println("3の倍数一覧");
-
-            dataList.stream()
-                    .filter(s -> s % 3 == 0)
-                    .map(s -> "[" + s + "]")
-                    .forEach(System.out::print);
-
-            System.out.println("");
-        }
-
-        
+ 
         outputAnyNumber(testList);
 
         // 奇数の一覧を昇順ソートして出力
-        
-        /**
-         * 引数で受け取った数値から奇数のみを昇順ソートして出力する。
-         * @param dataList 振り分け対象データ
-         */
-        public static void outputSortOddNumber(List<Integer> dataList) {
-
-            System.out.println("奇数一覧（昇順）");
-
-            dataList.stream()
-                    .filter(s -> s % 2 != 0)   // 奇数のみ
-                    .sorted()                 // 昇順ソート
-                    .map(s -> "[" + s + "]")
-                    .forEach(System.out::print);
-
-            System.out.println("");
-        }
-        
         outputSortOddNumber(testList);
 
     }
@@ -129,8 +94,30 @@ public class Problem01 {
         System.out.println("");
     }
 
-    public static void outputAnyNumber(List<Integer> dataList) {};
+    public static void outputAnyNumber(List<Integer> dataList) {
+    System.out.println("3の倍数一覧");
 
-    public static void outputSortOddNumber(List<Integer> dataList) {};
+    dataList.stream()
+        .filter(s -> s % 3 == 0)
+        .map(s -> "[" + s + "]")
+        .forEach(System.out::print);
+
+    System.out.println("");
+}
+    
+
+    public static void outputSortOddNumber(List<Integer> dataList) {
+    System.out.println("奇数一覧（昇順）");
+
+    dataList.stream()
+        .filter(s -> s % 2 != 0)   // 奇数のみ
+        .sorted()                 // 昇順ソート
+        .map(s -> "[" + s + "]")
+        .forEach(System.out::print);
+
+    System.out.println("");
+}
+    
+    
 
 }
