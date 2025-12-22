f756417b0f94e9317efd6dfd98b21a650267553f
中山裕人 <@DESKTOP-T0HONAA>
2025-12-22 17:32:39 +0900

    問題7　完了

--- first_traning/src/change/Problem07.java
+++ first_traning/src/change/Problem07.java
@@ -1,7 +1,6 @@
 package change;
 
 import hierarchy.sushi.Itamae;
-import hierarchy.sushi.Tray;
 
 // 回転寿司を注文する問題
 public class Problem07 {
@@ -20,40 +19,34 @@ public class Problem07 {
         itamae.order("うに");
         itamae.order("大トロ");
         itamae.order("まぐろ");
+        itamae.order("サーモン");
+        itamae.order("サーモン");
+        itamae.order("サーモン");
+        itamae.order("甘エビ");
 
         System.out.println("注文票-------------");
         itamae.payment();
         System.out.println("-------------------");
 
         /* -- ここから問題 -- */
+        
+     // Trayクラスを継承した新たな黄皿、及び緑皿を作れ
+     // なお、黄皿は1皿210円、緑皿は1皿420円とする。
 
         // 黄皿である「サーモン」を3皿、緑皿である「甘エビ」を1皿を追加注文せよ
-        // Trayクラスを継承した新たな黄皿、及び緑皿を作れ
-        // なお、黄皿は1皿210円、緑皿は1皿420円とする。
 
         // 黄皿である「サーモン」を3皿追加注文
-        itamae.order("サーモン");
+        
+        /*itamae.order("サーモン");
         itamae.order("サーモン");
         itamae.order("サーモン");
 
         // 緑皿である「甘エビ」を1皿追加注文
-        itamae.order("甘エビ");
+        itamae.order("甘エビ");*/
+    }
+}
         
 // Trayクラスを継承した新たな黄皿、及び緑皿を作れ
 // なお、黄皿は1皿210円、緑皿は1皿420円とする。
 //黄皿（1皿210円）
-class YellowTray extends Tray {
-	public YellowTray(String name) {
-     this.name = name;
-     this.price = 210;
- }
-}
 
-//緑皿（1皿420円）
-class GreenTray extends Tray {
- public GreenTray(String name) {
-     this.name = name;
-     this.price = 420;
- }
-}
-    }
---
+++ first_traning/src/hierarchy/sushi/YellowTray.java
@@ -0,0 +1,17 @@
+package hierarchy.sushi;
+
+/**
+ * 黄皿
+ */
+public class YellowTray extends Tray {
+
+    // 1皿の値段
+    int price = 210;
+
+    @Override
+    int getPrice() {
+        return price;
+    }
+
+
+}
