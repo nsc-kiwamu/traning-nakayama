e66305dfdf7637fa47aaff5e2f8b0506ec77c94b
中山裕人 <@DESKTOP-T0HONAA>
2026-01-05 14:10:12 +0900

    問題06　完了

--- first_traning/src/change/Problem06.java
+++ first_traning/src/change/Problem06.java
@@ -39,15 +39,20 @@ public class Problem06 {
 
             while (leftPointer <= rightPointer) {
                 // 左から基準値を超える要素を探す。（存在しない場合は基準値自体が対象となる）
-                while (left == 0/*　問題 「left == 0」を削除し、実装せよ*/) {
+            	//2025/1/5 NAYAYAMA add_st
+                while (array[leftPointer] < pivotData) {
+                //2025/1/5 NAYAYAMA add_end
                     leftPointer++;
                 }
                 // 右から基準値未満の要素を探す。（存在しない場合は基準値自体が対象となる）
-                while (left == 0/*　問題 「left == 0」を削除し、実装せよ*/) {
+                //2025/1/5 NAYAYAMA add_st
+                while (array[rightPointer] > pivotData) {
+                //2025/1/5 NAYAYAMA add_end
                     rightPointer--;
                 }
-
-                if (left == 0/*　問題 「left == 0」を削除し、実装せよ*/) {
+                //2025/1/5 NAYAYAMA add_st
+                if (leftPointer <= rightPointer) {
+                //2025/1/5 NAYAYAMA add_end
                     int tmp = array[leftPointer];
                     array[leftPointer] = array[rightPointer];
                     array[rightPointer] = tmp;
