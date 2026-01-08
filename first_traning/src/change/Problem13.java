dc30ecf9ff07f51e9f9ad1c5ee469abd560122a7
中山裕人 <@DESKTOP-T0HONAA>
2026-01-08 13:34:48 +0900

    問題13　完了

--- first_traning/src/change/Problem13.java
+++ first_traning/src/change/Problem13.java
@@ -6,10 +6,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import race.wrap.AllWrapper;
 import race.wrap.BottomWrapper;
 import race.wrap.SideWrapper;
 import race.wrap.StringWrapper;
-
+//2025/1/8 NAKAYAMA add_st
+import race.wrap.TopWrapper;
+//2025/1/8 NAKAYAMA add_end
 /**
  * 入出力の問題
  *
@@ -82,9 +85,19 @@ public class Problem13 {
         }
 
         // 横と下をラップする
+        //2025/1/8 NAKAYAMA add_st
+        StringWrapper top = new TopWrapper();
         StringWrapper side = new SideWrapper();
         StringWrapper bottom = new BottomWrapper();
-        System.out.println(bottom.wrap(side.wrap(inputStr, '*'), '*'));
+
+        System.out.println(
+            bottom.wrap(
+                side.wrap(
+                    top.wrap(inputStr, '*'),
+                '*'),
+            '*')
+        );
+        //2025/1/8 NAKAYAMA add_end
 
     }
 
@@ -129,11 +142,10 @@ public class Problem13 {
         }
 
 
-        // 横と下をラップする
-        StringWrapper side = new SideWrapper();
-        StringWrapper bottom = new BottomWrapper();
-        System.out.println(bottom.wrap(side.wrap(build.toString(), '|'), '-'));
-
+        //2025/1/8 NAKAYAMA add_st
+        StringWrapper all = new AllWrapper();
+        System.out.println(all.wrap(build.toString(), '*'));
+        //2025/1/8 NAKAYAMA add_end
     }
 
 }
---
+++ first_traning/src/race/wrap/TopWrapper.java
@@ -0,0 +1,31 @@
+package race.wrap;
+
+public class TopWrapper implements StringWrapper {
+
+    @Override
+    public String wrap(String target, char wrapChar) {
+
+        String[] lines = target.split(System.lineSeparator());
+        int maxLength = 0;
+
+        // 一番長い行を探す
+        for (String line : lines) {
+            if (maxLength < line.length()) {
+                maxLength = line.length();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        // 上の枠
+        for (int i = 0; i < maxLength + 2; i++) {
+            sb.append(wrapChar);
+        }
+        sb.append(System.lineSeparator());
+
+        // 元の文字列
+        sb.append(target);
+
+        return sb.toString();
+    }
+}
