b097a8d198b28e5c91a85b526d3117c830d81ce5
中山裕人 <@DESKTOP-T0HONAA>
2026-01-14 13:36:54 +0900

    問題16　完了 問題15　①、③完了

--- first_traning/src/io/rebuilding/MakeDirectoryRule.java
+++ first_traning/src/io/rebuilding/MakeDirectoryRule.java
@@ -11,13 +11,13 @@ import java.util.function.Function;
 
 /**
  * ディレクトリ構成の変更を実施するためのクラス
- *
  */
 public class MakeDirectoryRule implements RebuildRule {
+
     String targetName = null;
 
     private MakeDirectoryRule() {
-    };
+    }
 
     /**
      * コンストラクタ
@@ -29,14 +29,14 @@ public class MakeDirectoryRule implements RebuildRule {
 
     /**
      * コンストラクタで取得した文字列を含むファイルを対象とする。
-     * @param 判定対象のパス
+     * @param path 判定対象のパス
      */
     @Override
     public boolean isTarget(Path path) {
 
         boolean target = false;
 
-        // ファイル名が編成対象かどうかを返却する。
+        // ファイル名が編成対象かどうかを返却する
         if (path.toFile().getName().indexOf(targetName) != -1) {
             target = true;
         }
@@ -51,27 +51,38 @@ public class MakeDirectoryRule implements RebuildRule {
 
         // 引数のリスト分処理をする
         for (Path path : paths) {
+
             // 編集ルールで決められた通りに出力元、先を決める
             Path destPath = func.apply(path);
 
-            // 対象ファイル名からフォルダ名を設定
+            //2025/1/14 NAKAYAMA add_st
+            // 対象ファイル名から年月を取得し、月毎のフォルダを作成
             String outPutFileName = destPath.getFileName().toString();
             String[] fileNameList = outPutFileName.split(" ");
             String[] strYear = fileNameList[2].split("年");
             String year = strYear[0];
             String[] strMonth = strYear[1].split("月");
             String month = strMonth[0];
+
+            // 月が1桁の場合は0埋め
             if (month.length() == 1) {
                 month = "0" + month;
             }
 
-            // 月毎にフォルダを作成
-            String dirPath = Paths.get(destPath.getParent().toString(), year + month).toString();
+            // yyyyMM フォルダを作成
+            String dirPath = Paths.get(
+                    destPath.getParent().toString(),
+                    year + month
+            ).toString();
+
             File file = new File(dirPath);
             file.mkdir();
-            destPath = Paths.get(dirPath, outPutFileName);
 
-            // Mapからリストを取得する、取得できない場合はnewしたリストを取得
+            // 出力先パスを yyyyMM 配下に変更
+            destPath = Paths.get(dirPath, outPutFileName);
+            //2025/1/14 NAKAYAMA add_end
+
+            // Mapからリストを取得、存在しない場合は新規作成
             List<Path> srcList = retMap.getOrDefault(destPath, new ArrayList<Path>());
             srcList.add(path);
             retMap.put(destPath, srcList);
@@ -79,6 +90,4 @@ public class MakeDirectoryRule implements RebuildRule {
 
         return retMap;
     }
-
-
 }
